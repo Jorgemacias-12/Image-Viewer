6abc4750e93a82567a2f32a959aa1371e5f3b4c7
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-12-08 19:05:15 -0600

    Definiendo la UI, añadiendo componentes

--- src/frame.py
+++ src/frame.py
@@ -1,5 +1,57 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+import requests
+
+import utils
+
 def main():
-    return
+    root = Tk()
+    root.title("Image Viewer - JAMZ")
+    root.resizable(False, False)
+    
+    
+    menubar = Menu(root)
+   
+    folderMenu = Menu(menubar, tearoff=0)
+    folderMenu.add_command(label="Open Folder", command=None)
+    
+    menubar.add_cascade(label="File", menu=folderMenu)
+    
+    root.config(menu=menubar)
+    
+    imageView = Label(root,
+                      image=None,)
+    
+    infoView = Label(root, 
+                     text="Image None of None",
+                     fg="#fff",
+                     bg="#303030",
+                     font=("Roboto", 16, "bold"),
+                     bd=1,
+                     relief=SUNKEN,)
+    
+    prevButton = Button(root,
+                        text="<",
+                        fg="#fff",
+                        bg="#5600ff",
+                        padx=10,
+                        pady=10,
+                        font=("Roboto", 16, "bold"))
+    
+    nextButton = Button(root,
+                        text=">",
+                        fg="#fff",
+                        bg="#5600ff",
+                        padx=10,
+                        pady=10,
+                        font=("Roboto", 16, "bold"))
+    
+    prevButton.grid(column=0, row=0)
+    imageView.grid(column=1, row=0)
+    nextButton.grid(column=2, row=0)
+    infoView.grid(column=1, row=1, sticky=W+E)
+    
+    root.after(0, lambda: utils.initViewLabel(imageView))
+        
+    root.mainloop()
